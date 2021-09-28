d9cb215627999a10553f448d79d6947f4e954fb0
Diallo Mamadou Lamarana <lyve.diallo@gmail.com>
2021-09-28 22:22:20 +0000

    integrating delete update strategy

--- data-importer/constant.js
+++ data-importer/constant.js
@@ -1,9 +1,13 @@
 const constants = {
     QUERY_INSERT_TEI: "insert into tracker_entity_instance (id,age,certificate,code,local_id,nom,prenom,sexe,telephone,prefecture,sous_prefecture,village,quartier,profession,uid,region) values (nextval('sequence_generator'), $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14,$15) RETURNING id;",
-    QUERY_INSERT_EVENT: "insert into event (id,uid,date_vaccination,site_vaccination,type_vaccin,lot,tei_id,dose) values(nextval('sequence_generator'),$1,$2,$3,$4,$5,$6,$7)",
+    QUERY_UPDATE_TEI: "update tracker_entity_instance set age=$1,local_id=$2,nom=$3,prenom=$4,sexe=$5,telephone=$6,prefecture=$7,sous_prefecture=$8,village=$9,quartier=$10,profession=$11,region=$12 where uid=$13 RETURNING id;",
+    QUERY_INSERT_EVENT: "insert into event (id,uid,date_vaccination,site_vaccination,type_vaccin,lot,tei_id,dose) values(nextval('sequence_generator'),$1,$2,$3,$4,$5,$6,$7);",
     QUERY_UPDATE_EVENT: "update event set date_vaccination=$1,site_vaccination=$2,type_vaccin=$3,lot=$4 where uid = $5;",
-    QUERY_SELECT_TEI: "select * from tracker_entity_instance where uid =$1",
-    QUERY_SELECT_EVENT: "select * from event where uid =$1",
+    QUERY_SELECT_TEI: "select * from tracker_entity_instance where uid =$1;",
+    QUERY_SELECT_EVENT: "select * from event where uid = $1;",
+    QUERY_DELETE_TEI : "delete from tracker_entity_instance where uid = $1;",
+    QUERY_DELETE_EVENT : "delete from event where uid = $1;",
+    STATUS_DE : "ENZzol6OWdD"
 };
 
 
--- data-importer/entry.js
+++ data-importer/entry.js
@@ -1,11 +1,11 @@
-const { dhis2 } = require('./dhis2/dhis2');
+const { dhis2} = require('./dhis2/dhis2');
 const config = require('config')
 const connectDB = require('./config/db');
 const cron = require("node-cron");
 const { constants } = require('./constant');
 
 const REQUEST = config.get('dhis2.query');
-const SIZE = 200;
+const SIZE = 500;
 
 
 
@@ -13,18 +13,25 @@ const postgres = async () => {
 
 
     let page = 1;
-    let pager = { pageCount: 1 };
+    let pager = {
+        pageCount: 1
+    };
 
     const client = await connectDB();
-
-    while (page <= pager.pageCount) {
+    
+     while (page <= pager.pageCount) {
 
         let FULL_URL = `${REQUEST}&pageSize=${SIZE}&page=${page}`;
 
         const response = await dhis2.get(FULL_URL);
         const rows = response.data.rows;
-        processing(rows, client);
-        pager = { page, total, pageSize, pageCount } = response.data.metaData.pager
+        await processing(rows, client);
+        pager = {
+            page,
+            total,
+            pageSize,
+            pageCount
+        } = response.data.metaData.pager
         console.log(pager);
         page = page + 1;
     }
@@ -33,45 +40,99 @@ const postgres = async () => {
 }
 
 
-const processing = async (rows, client) =>{
+const processing = async (rows, client) => {
+   
+    let insertTei = 0;
+    let insertEvent = 0;
+    let updateEvent = 0;
     for (const v_event of rows) {
+
         try {
 
             const row = formatEvent(v_event);
 
-            // check if the current tei is in the database;
-            const dbTei = await client.query(constants.QUERY_SELECT_TEI, [row.trackedEntityInstanceUid]);
-
             let trackedEntityInstanceId = 0;
             let tei = [];
+            // check row status
 
-            if (dbTei.rows && dbTei.rows.length > 0) {
-                trackedEntityInstanceId = dbTei.rows[0].id;
-            } else {
-                // insert a trackedEntityInstance
-                const values = [row.age, row.naissance, 234, row.localid, row.nom, row.prenom, row.sexe, row.telephone, row.prefecture, row.sousPrefecture, "", row.quartier, row.profession, row.trackedEntityInstanceUid,row.region];
-                tei = await client.query(constants.QUERY_INSERT_TEI, values);
-                trackedEntityInstanceId = tei.rows[0].id;
+            
+           
+             // if delete -> delete tei and event
+            if (row.statut === 'Delete') {
+                await client.query(constants.QUERY_DELETE_EVENT, [row.eventUid]);
+                await client.query(constants.QUERY_DELETE_TEI, [row.trackedEntityInstanceUid]);
+                // delete de tei
+                await dhis2.delete(`/trackedEntityInstances/${row.trackedEntityInstanceUid}`)
+                console.log("Delete tei ", row.trackedEntityInstanceUid );
+            } else if(row.statut !== 'Done') {  // if (row.statut === 'Update' || row.statut === 'OK')
+
+               
+                // check if the current tei is in the database;
+                const dbTei = await client.query(constants.QUERY_SELECT_TEI, [row.trackedEntityInstanceUid]);
+                
+
+                if (dbTei.rows && dbTei.rows.length > 0) {
+                    trackedEntityInstanceId = dbTei.rows[0].id;
+                    console.log("updating ", row.trackedEntityInstanceUid)
+                    const values = [row.age, row.localid, row.nom, row.prenom, row.sexe, row.telephone, row.prefecture, row.sousPrefecture, "", row.quartier, row.profession, row.region, row.trackedEntityInstanceUid];
+                    tei = await client.query(constants.QUERY_UPDATE_TEI, values);
+                    console.log("update tei ", row.trackedEntityInstanceUid );
+                    //update tei
+                } else {
+                    // insert a trackedEntityInstance
+                    const values = [row.age, row.naissance, 234, row.localid, row.nom, row.prenom, row.sexe, row.telephone, row.prefecture, row.sousPrefecture, "", row.quartier, row.profession, row.trackedEntityInstanceUid, row.region];
+                    tei = await client.query(constants.QUERY_INSERT_TEI, values);
+                    trackedEntityInstanceId = tei.rows[0].id;
+                    insertTei++;
+                    console.log("insert tei ", row.trackedEntityInstanceUid );
+
+                }
+
+                // check current event is already in the database
+                let eventText = "";
+                let eventValues = [];
+                const dbEvent = await client.query(constants.QUERY_SELECT_EVENT, [row.eventUid]);
+                if (dbEvent.rows && dbEvent.rows.length > 0) {
+                    // update linked event
+                    eventText = constants.QUERY_UPDATE_EVENT;
+                    eventValues = [row.dateVaccination, row.siteVaccination, row.typeVaccin, row.numeroLot, row.eventUid];
+                    updateEvent++
+                } else {
+                    // insert linked event (vaccination)
+                    eventText = constants.QUERY_INSERT_EVENT;
+                    eventValues = [row.eventUid, row.dateVaccination, row.siteVaccination, row.typeVaccin, row.numeroLot, trackedEntityInstanceId, row.numeroDose];
+                    insertEvent++;
+                }
+                await client.query(eventText, eventValues);
+
+                // get current event to set it DONE
+                
+                const event = await dhis2.get(`/events/${row.eventUid}.json`);
+                // Check status
+                const status = event.data.dataValues.filter(e => e.dataElement === constants.STATUS_DE);
+
+                if (status && status.length === 0) { 
+                    event.data.dataValues.push({
+                        created: new Date(),
+                        value: "Done",
+                        dataElement: constants.STATUS_DE,
+                        providedElsewhere: false,
+                        storedBy: 'didate'
+                    });
+                }else{
+                    event.data.dataValues.filter(e => e.dataElement === constants.STATUS_DE)[0].value = "Done"
+                }
+
+                await dhis2.put(`/events/${row.eventUid}`, JSON.stringify(event.data));
+
             }
 
-            // check current event is already in the database 
-            let eventText = "";
-            let eventValues = [];
-            const dbEvent = await client.query(constants.QUERY_SELECT_EVENT, [row.eventUid]);
-            if (dbEvent.rows && dbEvent.rows.length > 0) {
-                // update linked event
-                eventText = constants.QUERY_UPDATE_EVENT;
-                eventValues = [row.dateVaccination, row.siteVaccination, row.typeVaccin, row.numeroLot, row.eventUid];
-            } else {
-                // insert linked event (vaccination)
-                eventText = constants.QUERY_INSERT_EVENT;
-                eventValues = [row.eventUid, row.dateVaccination, row.siteVaccination, row.typeVaccin, row.numeroLot, trackedEntityInstanceId, row.numeroDose];
-            }
-            await client.query(eventText, eventValues);
         } catch (error) {
             console.log(error);
         }
     };
+
+    console.log(`Insert Tei : ${insertTei}, Insert Event : ${insertEvent}, Update Event : ${updateEvent}`)
 }
 
 
@@ -99,6 +160,7 @@ formatEvent = (v_event) => {
         typeVaccin: v_event[30],
         siteVaccination: v_event[31] === "AUTRES" ? v_event[32] : v_event[31],
         dateProchainDose: v_event[33],
+        statut: v_event[34],
         age: calcAge(new Date(v_event[17]), v_event[26])
     };
 
@@ -107,10 +169,8 @@ formatEvent = (v_event) => {
 }
 
 function calcAge(birthDate, vaccDate) {
-
-    var birthday = new Date(birthDate);
     //return ~~((new Date(vaccDate) - birthday) / (31557600000));
-    return new Date(vaccDate).getFullYear() - birthday.getFullYear();
+    return new Date(vaccDate).getFullYear() - birthDate.getFullYear();
 }
 
 postgres();
